13185196fc0a1e1e9b278959a2c18483937af02e
Tudor <sam.tudor@capgemini.com>
2020-07-15 11:06:00 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -28,3 +25,7 @@ Route::get('/books', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
