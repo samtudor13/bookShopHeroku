b2c2d1e2a509f82dd96386a24c26a04c064a7451
Tudor <sam.tudor@capgemini.com>
2020-07-15 10:54:31 +0100

    initial commit

---
+++ resources/views/books.blade.php
@@ -0,0 +1,30 @@
+<!--Adds the login and registration bar to the top of the page-->
+@extends('layouts.app')
+
+<!--Form below for the add animal to the data base-->
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UFT-8">
+  <meta name="viewport" content="width-device-width, initial-scale-1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie-edge">
+  <title>Add Animal</title>
+</head>
+<body>
+  <!--Container for the form to sit within-->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">Add Animal</div>
+            <div class="panel-body">
+
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</body>
+</html>
+@endsection
